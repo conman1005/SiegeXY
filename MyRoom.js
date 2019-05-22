da8dba34e8a066b6086071457f089f14b9c9ed7c
conman1005 <connercullity@live.ca>
2019-05-22 11:36:21 -0400

    added colyseus

---
+++ MyRoom.js
@@ -0,0 +1,76 @@
+const colyseus = require('colyseus');
+
+class Player {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class RoomState {
+  constructor () {
+    this.players = {};
+  }
+
+  addPlayer (client) {
+    this.players[ client.sessionId ] = new Player(rand(0, 500), rand(0, 500));
+  }
+
+  removePlayer (client) {
+    delete this.players[ client.sessionId ];
+  }
+
+  movePlayer (client, dir) {
+      console.log(client.sessionId, dir);
+    if (dir === "left") {
+      this.players[ client.sessionId ].x -= 1;
+
+    } else if (dir === "right") {
+      this.players[ client.sessionId ].x += 1;
+    }
+    else if (dir === "up") {
+      this.players[ client.sessionId ].y -= 1;
+    }
+    else if (dir === "down") {
+      this.players[ client.sessionId ].y += 1;
+    }
+  }
+  moveX (client, dir) {
+      this.players[client.sessionId].x = 0 - dir;
+  }
+  moveY (client, dir) {
+      this.players[client.sessionId].y = 0 - dir;
+  }
+}
+
+exports.MyRoom = class extends colyseus.Room {
+
+  onInit (options) {
+    this.setState(new RoomState());
+    console.log("room initialized", options)
+  }
+
+  onJoin (client) {
+    this.state.addPlayer(client);
+    console.log("client joined", client.sessionId)
+  }
+
+  onLeave (client) {
+    this.state.removePlayer(client);
+  }
+
+  onMessage (client, data) {
+    console.log(data)
+    if (data.type && data.type=='move') {
+      this.state.movePlayer(client, data.dir);
+    } else if (data.type && data.type=='moveX') {
+        this.state.moveX(client, data.dir);
+    } else if (data.type && data.type=='moveY') {
+        this.state.moveY(client, data.dir);
+    }
+  }
+}
+
+function rand(min, max){
+	return Math.floor(Math.random() * (+max - +min)) + +min
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const colyseus = require('colyseus');
+const monitor = require("@colyseus/monitor").monitor;
+
+const MyRoom = require('./MyRoom').MyRoom;
+colyseus.serialize(colyseus.FossilDeltaSerializer)(MyRoom);
+
+const port = process.env.PORT || 2567;
+const app = express();
+
+const server = http.createServer(app);
+const gameServer = new colyseus.Server({ server });
+
+// register your room handlers
+gameServer.register('my_room', MyRoom);
+
+// Register colyseus monitor AFTER registering your room handlers
+app.use("/colyseus", monitor(gameServer));
+app.use('/', express.static(path.join(__dirname, "static")));
+
+
+gameServer.listen(port);
+console.log(`Listening on ws://localhost:${ port }`)
